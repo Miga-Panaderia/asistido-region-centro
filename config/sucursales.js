04891a2dfd090ba8afef62ff5f89ae9d77679b5b
Miga-Panaderia <j31121985@gmail.com>
2026-08-31 16:57:50 -0300

    Add files via upload

--- config/sucursales.js
+++ config/sucursales.js
@@ -1,475 +1,98 @@
-// MIGA - Configuración de zonas y sucursales
-// Generado a partir de "Zona y Sucursales.xlsx".
-// Para agregar una sucursal en el futuro, actualizar este archivo y volver a publicar en GitHub.
-
 window.MIGA_CONFIG = {
-  "version": "1.0",
-  "zones": [
+  version: 'multizona-2026-08-31',
+  zones: [
     {
-      "id": "neuquen-interior",
-      "name": "NEUQUEN INTERIOR",
-      "branches": [
-        "050",
-        "076",
-        "172",
-        "182",
-        "188",
-        "213",
-        "228",
-        "230",
-        "234",
-        "247",
-        "259",
-        "266",
-        "356",
-        "357"
-      ]
+      id: 'neuquen-interior',
+      name: 'NEUQUEN INTERIOR',
+      branches: ['050','076','172','182','188','213','228','230','234','247','259','266','356','357']
     },
     {
-      "id": "neuquen-ciudad",
-      "name": "NEUQUEN CIUDAD",
-      "branches": [
-        "006",
-        "068",
-        "162",
-        "225",
-        "226",
-        "229",
-        "232",
-        "233",
-        "235",
-        "236",
-        "237",
-        "239",
-        "240",
-        "248"
-      ]
+      id: 'neuquen-ciudad',
+      name: 'NEUQUEN CIUDAD',
+      branches: ['006','068','162','225','226','229','232','233','235','236','237','239','240','248']
     },
     {
-      "id": "rio-negro",
-      "name": "RIO NEGRO",
-      "branches": [
-        "007",
-        "010",
-        "042",
-        "048",
-        "051",
-        "053",
-        "092",
-        "164",
-        "195",
-        "238",
-        "241",
-        "242",
-        "243",
-        "246",
-        "265",
-        "363"
-      ]
+      id: 'rio-negro',
+      name: 'RIO NEGRO',
+      branches: ['007','010','042','048','051','053','092','164','195','238','241','242','243','246','265','363']
     },
     {
-      "id": "trelew",
-      "name": "TRELEW",
-      "branches": [
-        "012",
-        "013",
-        "014",
-        "016",
-        "044",
-        "045",
-        "134",
-        "167",
-        "181",
-        "207"
-      ]
+      id: 'trelew',
+      name: 'TRELEW',
+      branches: ['012','013','014','016','044','045','134','167','181','207']
     },
     {
-      "id": "viedma",
-      "name": "VIEDMA",
-      "branches": [
-        "011",
-        "046",
-        "047",
-        "069",
-        "163",
-        "208",
-        "215",
-        "216"
-      ]
+      id: 'viedma',
+      name: 'VIEDMA',
+      branches: ['011','046','047','069','163','208','215','216']
     }
   ],
-  "branches": {
-    "050": {
-      "code": "050",
-      "name": "ZAPALA",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "076": {
-      "code": "076",
-      "name": "PLAZA HUINCUL",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "172": {
-      "code": "172",
-      "name": "RINCON DE LOS SAUCES",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "182": {
-      "code": "182",
-      "name": "PLOTTIER",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "188": {
-      "code": "188",
-      "name": "CATRIEL",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "213": {
-      "code": "213",
-      "name": "CENTENARIO 1",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "228": {
-      "code": "228",
-      "name": "CUTRAL CO 2",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "230": {
-      "code": "230",
-      "name": "AUTOSERVICIO ZAPALA 2",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "234": {
-      "code": "234",
-      "name": "AUTOSERVICIO PLOTTIER 2",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "247": {
-      "code": "247",
-      "name": "AUTOSERVICIO ZAPALA 3",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "259": {
-      "code": "259",
-      "name": "CUTRAL CO",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "266": {
-      "code": "266",
-      "name": "ANELO",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "356": {
-      "code": "356",
-      "name": "CENTENARIO II",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "357": {
-      "code": "357",
-      "name": "SAN PATRICIO DEL CHAÑAR",
-      "zoneId": "neuquen-interior",
-      "zone": "NEUQUEN INTERIOR"
-    },
-    "006": {
-      "code": "006",
-      "name": "BAHIA BLANCA",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "068": {
-      "code": "068",
-      "name": "HIPERMERCADO NEUQUEN",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "162": {
-      "code": "162",
-      "name": "BELGRANO 1",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "225": {
-      "code": "225",
-      "name": "AVD SAN JUAN",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "226": {
-      "code": "226",
-      "name": "NEUQUEN CENTRO",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "229": {
-      "code": "229",
-      "name": "NEUQUEN OESTE 2",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "232": {
-      "code": "232",
-      "name": "FAVA",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "233": {
-      "code": "233",
-      "name": "BELGRANO 3",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "235": {
-      "code": "235",
-      "name": "ALDERETE",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "236": {
-      "code": "236",
-      "name": "NEUQUEN OESTE 1",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "237": {
-      "code": "237",
-      "name": "ALTA BARDA",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "239": {
-      "code": "239",
-      "name": "AVDA TRABAJADOR",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "240": {
-      "code": "240",
-      "name": "BELGRANO 2",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "248": {
-      "code": "248",
-      "name": "ANTARTIDA ARGENTINA",
-      "zoneId": "neuquen-ciudad",
-      "zone": "NEUQUEN CIUDAD"
-    },
-    "007": {
-      "code": "007",
-      "name": "CIPOLLETTI",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "010": {
-      "code": "010",
-      "name": "GENERAL ROCA",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "042": {
-      "code": "042",
-      "name": "AUTOSERVICIO GRAL ROCA 2",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "048": {
-      "code": "048",
-      "name": "VILLA REGINA",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "051": {
-      "code": "051",
-      "name": "CIPOLLETTI 2",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "053": {
-      "code": "053",
-      "name": "ALLEN",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "092": {
-      "code": "092",
-      "name": "GENERAL ROCA 3",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "164": {
-      "code": "164",
-      "name": "CIPOLLETTI 3",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "195": {
-      "code": "195",
-      "name": "GENERAL ROCA 4",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "238": {
-      "code": "238",
-      "name": "AUTOSERVICIO CIPOLLETTI 4",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "241": {
-      "code": "241",
-      "name": "GRAL ROCA CENTRO 5",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "242": {
-      "code": "242",
-      "name": "AUTOSERVICIO CINCO SALTOS 2",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "243": {
-      "code": "243",
-      "name": "AUTOSERVICIO CIPOLLETTI 5",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "246": {
-      "code": "246",
-      "name": "AUTOSERVICIO VILLA REGINA 2",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "265": {
-      "code": "265",
-      "name": "CINCO SALTOS 1",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "363": {
-      "code": "363",
-      "name": "FERNANDEZ ORO",
-      "zoneId": "rio-negro",
-      "zone": "RIO NEGRO"
-    },
-    "012": {
-      "code": "012",
-      "name": "TRELEW",
-      "zoneId": "trelew",
-      "zone": "TRELEW"
-    },
-    "013": {
-      "code": "013",
-      "name": "RAWSON",
-      "zoneId": "trelew",
-      "zone": "TRELEW"
-    },
-    "014": {
-      "code": "014",
-      "name": "PUERTO MADRYN",
-      "zoneId": "trelew",
-      "zone": "TRELEW"
-    },
-    "016": {
-      "code": "016",
-      "name": "TRELEW FONTANA",
-      "zoneId": "trelew",
-      "zone": "TRELEW"
-    },
-    "044": {
-      "code": "044",
-      "name": "AUTOSERV.3 TRELEW",
-      "zoneId": "trelew",
-      "zone": "TRELEW"
-    },
-    "045": {
-      "code": "045",
-      "name": "AUTOSERVICIO PTO.MADRYN",
-      "zoneId": "trelew",
-      "zone": "TRELEW"
-    },
-    "134": {
-      "code": "134",
-      "name": "TRELEW 25 DE MAYO",
-      "zoneId": "trelew",
-      "zone": "TRELEW"
-    },
-    "167": {
-      "code": "167",
-      "name": "PUERTO MADRYN 3",
-      "zoneId": "trelew",
-      "zone": "TRELEW"
-    },
-    "181": {
-      "code": "181",
-      "name": "PLAYA UNION",
-      "zoneId": "trelew",
-      "zone": "TRELEW"
-    },
-    "207": {
-      "code": "207",
-      "name": "GAIMAN",
-      "zoneId": "trelew",
-      "zone": "TRELEW"
-    },
-    "011": {
-      "code": "011",
-      "name": "VIEDMA",
-      "zoneId": "viedma",
-      "zone": "VIEDMA"
-    },
-    "046": {
-      "code": "046",
-      "name": "SAN ANTONIO OESTE",
-      "zoneId": "viedma",
-      "zone": "VIEDMA"
-    },
-    "047": {
-      "code": "047",
-      "name": "VIEDMA 2",
-      "zoneId": "viedma",
-      "zone": "VIEDMA"
-    },
-    "069": {
-      "code": "069",
-      "name": "C. DE PATAGONES",
-      "zoneId": "viedma",
-      "zone": "VIEDMA"
-    },
-    "163": {
-      "code": "163",
-      "name": "SIERRA GRANDE",
-      "zoneId": "viedma",
-      "zone": "VIEDMA"
-    },
-    "208": {
-      "code": "208",
-      "name": "LAS GRUTAS",
-      "zoneId": "viedma",
-      "zone": "VIEDMA"
-    },
-    "215": {
-      "code": "215",
-      "name": "RIO COLORADO",
-      "zoneId": "viedma",
-      "zone": "VIEDMA"
-    },
-    "216": {
-      "code": "216",
-      "name": "CHOELE CHOEL",
-      "zoneId": "viedma",
-      "zone": "VIEDMA"
-    }
+  branches: {
+    '006': {code:'006', name:'BAHIA BLANCA', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '068': {code:'068', name:'HIPERMERCADO NEUQUEN', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '162': {code:'162', name:'BELGRANO 1', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '225': {code:'225', name:'AVD SAN JUAN', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '226': {code:'226', name:'NEUQUEN CENTRO', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '229': {code:'229', name:'NEUQUEN OESTE 2', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '232': {code:'232', name:'FAVA', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '233': {code:'233', name:'BELGRANO 3', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '235': {code:'235', name:'ALDERETE', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '236': {code:'236', name:'NEUQUEN OESTE 1', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '237': {code:'237', name:'ALTA BARDA', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '239': {code:'239', name:'AVDA TRABAJADOR', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '240': {code:'240', name:'BELGRANO 2', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+    '248': {code:'248', name:'ANTARTIDA ARGENTINA', zoneId:'neuquen-ciudad', zone:'NEUQUEN CIUDAD'},
+
+    '050': {code:'050', name:'ZAPALA', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '076': {code:'076', name:'PLAZA HUINCUL', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '172': {code:'172', name:'RINCON DE LOS SAUCES', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '182': {code:'182', name:'PLOTTIER', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '188': {code:'188', name:'CATRIEL', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '213': {code:'213', name:'CENTENARIO 1', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '228': {code:'228', name:'CUTRAL CO 2', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '230': {code:'230', name:'AUTOSERVICIO ZAPALA 2', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '234': {code:'234', name:'AUTOSERVICIO PLOTTIER 2', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '247': {code:'247', name:'AUTOSERVICIO ZAPALA 3', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '259': {code:'259', name:'CUTRAL CO', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '266': {code:'266', name:'AÑELO', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '356': {code:'356', name:'CENTENARIO II', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+    '357': {code:'357', name:'SAN PATRICIO DEL CHAÑAR', zoneId:'neuquen-interior', zone:'NEUQUEN INTERIOR'},
+
+    '007': {code:'007', name:'CIPOLLETTI', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '010': {code:'010', name:'GENERAL ROCA', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '042': {code:'042', name:'AUTOSERVICIO GRAL ROCA 2', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '048': {code:'048', name:'VILLA REGINA', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '051': {code:'051', name:'CIPOLLETTI 2', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '053': {code:'053', name:'ALLEN', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '092': {code:'092', name:'GENERAL ROCA 3', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '164': {code:'164', name:'CIPOLLETTI 3', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '195': {code:'195', name:'GENERAL ROCA 4', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '238': {code:'238', name:'AUTOSERVICIO CIPOLLETTI 4', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '241': {code:'241', name:'GRAL ROCA CENTRO 5', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '242': {code:'242', name:'AUTOSERVICIO CINCO SALTOS 2', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '243': {code:'243', name:'AUTOSERVICIO CIPOLLETTI 5', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '246': {code:'246', name:'AUTOSERVICIO VILLA REGINA 2', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '265': {code:'265', name:'CINCO SALTOS 1', zoneId:'rio-negro', zone:'RIO NEGRO'},
+    '363': {code:'363', name:'FERNANDEZ ORO', zoneId:'rio-negro', zone:'RIO NEGRO'},
+
+    '012': {code:'012', name:'TRELEW', zoneId:'trelew', zone:'TRELEW'},
+    '013': {code:'013', name:'RAWSON', zoneId:'trelew', zone:'TRELEW'},
+    '014': {code:'014', name:'PUERTO MADRYN', zoneId:'trelew', zone:'TRELEW'},
+    '016': {code:'016', name:'TRELEW FONTANA', zoneId:'trelew', zone:'TRELEW'},
+    '044': {code:'044', name:'AUTOSERV.3 TRELEW', zoneId:'trelew', zone:'TRELEW'},
+    '045': {code:'045', name:'AUTOSERVICIO PTO.MADRYN', zoneId:'trelew', zone:'TRELEW'},
+    '134': {code:'134', name:'TRELEW 25 DE MAYO', zoneId:'trelew', zone:'TRELEW'},
+    '167': {code:'167', name:'PUERTO MADRYN 3', zoneId:'trelew', zone:'TRELEW'},
+    '181': {code:'181', name:'PLAYA UNION', zoneId:'trelew', zone:'TRELEW'},
+    '207': {code:'207', name:'GAIMAN', zoneId:'trelew', zone:'TRELEW'},
+
+    '011': {code:'011', name:'VIEDMA', zoneId:'viedma', zone:'VIEDMA'},
+    '046': {code:'046', name:'SAN ANTONIO OESTE', zoneId:'viedma', zone:'VIEDMA'},
+    '047': {code:'047', name:'VIEDMA 2', zoneId:'viedma', zone:'VIEDMA'},
+    '069': {code:'069', name:'C. DE PATAGONES', zoneId:'viedma', zone:'VIEDMA'},
+    '163': {code:'163', name:'SIERRA GRANDE', zoneId:'viedma', zone:'VIEDMA'},
+    '208': {code:'208', name:'LAS GRUTAS', zoneId:'viedma', zone:'VIEDMA'},
+    '215': {code:'215', name:'RIO COLORADO', zoneId:'viedma', zone:'VIEDMA'},
+    '216': {code:'216', name:'CHOELE CHOEL', zoneId:'viedma', zone:'VIEDMA'}
   }
 };
